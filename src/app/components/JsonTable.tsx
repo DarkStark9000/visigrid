6239498223be769a681be03d52c637376db2ae74
Debarshi Das <debarshi.a.das@pwc.com>
2023-07-25 17:27:51 +0530

    ✅Added Pagination so that app doesnt crash when data points more than > 100K ✅Added inline edit functionality - the changes persists in memory and after page changes also. ✅Chaged from <table> tag to <div> using grid, making it vertically scrollable.

--- src/app/components/JsonTable.tsx
+++ src/app/components/JsonTable.tsx
@@ -1,33 +1,71 @@
-// app/components/JsonTable.tsx
-
-import { FC } from "react";
+import { FC, useState } from "react";
+import Pagination from "./Pagination";
 
 interface JsonTableProps {
   data: any[];
 }
 
 const JsonTable: FC<JsonTableProps> = ({ data }) => {
-  const [columns, ...rows] = data;
+  const [tableData, setTableData] = useState(data);
+  const [currentPage, setCurrentPage] = useState(0);
+  const pageSize = 100; // Number of rows per page
+
+  const columns = tableData[0];
+  const rows = tableData.slice(1);
+  const totalPages = Math.ceil(rows.length / pageSize);
+
+  const displayedRows = rows.slice(
+    currentPage * pageSize,
+    (currentPage + 1) * pageSize
+  );
+
+  const handleCellChange = (
+    rowIndex: number,
+    colIndex: number,
+    value: string
+  ) => {
+    const updatedRows = [...rows];
+    updatedRows[rowIndex][colIndex] = value;
+    setTableData([columns, ...updatedRows]);
+  };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          {columns.map((column: any, index: any) => (
-            <th key={index}>{column}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {row.map((value: any, colIndex: any) => (
-              <td key={colIndex}>{value}</td>
-            ))}
-          </tr>
+    <>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${columns.length}, 1fr)`,
+          gap: "10px",
+        }}
+      >
+        {columns.map((column: any, index: any) => (
+          <div
+            key={index}
+            style={{ fontWeight: "bold", borderBottom: "1px solid black" }}
+          >
+            {column}
+          </div>
         ))}
-      </tbody>
-    </table>
+        {displayedRows.map((row, rowIndex) =>
+          row.map((value: any, colIndex: any) => (
+            <div key={`${rowIndex}-${colIndex}`}>
+              <input
+                type="text"
+                value={value}
+                onChange={(e) =>
+                  handleCellChange(rowIndex, colIndex, e.target.value)
+                }
+              />
+            </div>
+          ))
+        )}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
+    </>
   );
 };
 
