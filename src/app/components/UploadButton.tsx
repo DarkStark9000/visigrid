de2a16546a5c32014259a00e769eadcd0f8ce07b
Debarshi Das <debarshi.a.das@pwc.com>
2023-07-31 17:52:14 +0530

    ✅ UI Error handling for mismatched file extension.

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import Papa from "papaparse";
 
 interface UploadButtonProps {
@@ -12,6 +12,8 @@ const UploadButton: FC<UploadButtonProps> = ({
   resetData,
   isDataLoaded,
 }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const handleUpload = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     event.target.value = ""; // Reset the value of the input
@@ -47,9 +49,7 @@ const UploadButton: FC<UploadButtonProps> = ({
               },
             });
           } else {
-            console.error(
-              "Invalid file type. Please upload a JSON or CSV file."
-            );
+            setIsModalOpen(true);
           }
         } catch (e) {
           console.error("Could not parse file.");
@@ -60,20 +60,61 @@ const UploadButton: FC<UploadButtonProps> = ({
   };
 
   return (
-    <label
-      className="inline-block px-8 py-4 bg-indigo-700 text-white cursor-pointer rounded text-xl font-normal tracking-normal hover:bg-blue-600"
-      style={{
-        boxShadow: `rgba(0, 0, 0, 0.24) 0px 3px 8px`,
-      }}
-    >
-      ↑ Open File
-      <input
-        type="file"
-        accept=".json,.csv"
-        onChange={handleUpload}
-        className="hidden"
-      />
-    </label>
+    <>
+      <label
+        className="inline-block px-8 py-4 bg-indigo-700 text-white cursor-pointer rounded text-xl font-normal tracking-normal hover:bg-blue-600"
+        style={{
+          boxShadow: `rgba(0, 0, 0, 0.24) 0px 3px 8px`,
+        }}
+      >
+        ↑ Open File
+        <input
+          type="file"
+          accept=".json,.csv"
+          onChange={handleUpload}
+          className="hidden"
+        />
+      </label>
+      {isModalOpen && (
+        <div className="fixed z-50 inset-0 flex items-center justify-center">
+          <div className="absolute w-full h-full bg-gray-500 opacity-50"></div>
+          <div className="bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+            <div className="py-4 text-left px-6">
+              <div className="flex justify-between items-center pb-3">
+                <p className="text-2xl font-semibold text-red-600">
+                  Invalid File Type
+                </p>
+                <div
+                  className="cursor-pointer z-50"
+                  onClick={() => setIsModalOpen(false)}
+                >
+                  <svg
+                    className="fill-current text-black"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                  >
+                    <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                  </svg>
+                </div>
+              </div>
+              <p className="text-base font-medium tracking-tight">
+                Please upload a JSON or CSV file.
+              </p>
+              <div className="flex justify-end pt-2">
+                <button
+                  className="text-base font-semibold tracking-wide hover: outline-blue-700 modal-close py-2 px-6 bg-blue-500 p-2 rounded-sm text-white "
+                  onClick={() => setIsModalOpen(false)}
+                >
+                  Okay
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
