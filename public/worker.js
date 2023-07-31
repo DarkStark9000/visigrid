724b91899642b12f9381946b867a5b95603a208b
Debarshi Das <debarshi.a.das@pwc.com>
2023-08-01 04:09:59 +0530

    🔧tiny perf

--- public/worker.js
+++ public/worker.js
@@ -1,14 +1,38 @@
+// self.onmessage = ({ data: { rows, colsPerPage } }) => {
+//   const columns = rows[0].slice(0, colsPerPage);
+//   const rowsData = rows.slice(1);
+//   let newUniqueValues = Array(columns.length)
+//     .fill(null)
+//     .map(() => new Set());
+//   for (let i = 0; i < rowsData.length; i++) {
+//     for (let j = 0; j < rowsData[i].length; j++) {
+//       newUniqueValues[j].add(rowsData[i][j]);
+//     }
+//   }
+//   const result = newUniqueValues.map((set) => Array.from(set));
+//   self.postMessage(result);
+// };
+
 self.onmessage = ({ data: { rows, colsPerPage } }) => {
   const columns = rows[0].slice(0, colsPerPage);
   const rowsData = rows.slice(1);
-  let newUniqueValues = Array(columns.length)
-    .fill(null)
-    .map(() => new Set());
+
+  let newUniqueValues = [];
+  for (let i = 0; i < columns.length; i++) {
+    newUniqueValues[i] = new Set();
+  }
+
   for (let i = 0; i < rowsData.length; i++) {
     for (let j = 0; j < rowsData[i].length; j++) {
-      newUniqueValues[j].add(rowsData[i][j]);
+      // Convert values to strings to optimize memory usage
+      newUniqueValues[j].add(String(rowsData[i][j]));
     }
   }
-  const result = newUniqueValues.map((set) => Array.from(set));
+
+  const result = [];
+  for (let i = 0; i < newUniqueValues.length; i++) {
+    result[i] = [...newUniqueValues[i]];
+  }
+
   self.postMessage(result);
 };
